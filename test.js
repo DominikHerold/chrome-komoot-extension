f13b52e7dc58b0ce128ab540e5205736bd1b76d8
Hannes.Schulze01 <Hannes.Schulze01@E0305722.internal.vodafone.com>
2018-10-18 13:14:22 +0200

    First Version

---
+++ test.js
@@ -0,0 +1,62 @@
+window.onload = function () {
+    var scripts = document.getElementsByTagName("script");
+    var findRe = /(kmtBoot.setProps)/;
+    var jsonRe = /kmtBoot.setProps\("(.*)"\)/;
+    for (i = 0; i < scripts.length; i++) {
+        var test = scripts[i].innerHTML.match(findRe);
+        if (test && test.length > 0)
+        {
+            var jsonTest = scripts[i].innerHTML.match(jsonRe);
+            
+            
+            if (jsonTest && jsonTest.length > 0) {
+                var myjson = jsonTest[1];
+                myjson = myjson.replace(/\\"/g, '"');
+                myjson = myjson.replace(/"*\\\\\\"*"/g, '"');
+                
+                myjson = myjson.replace(/"{/g, '{');
+                myjson = myjson.replace(/(}"),"d/g, '},"d');
+                myjson = myjson.replace(/\\\\"/g, '"');
+                var mydata = myjson.match(/"coordinates":{"items":\[(.*?)]/g)
+                var data = "{" + mydata[0]+ "}}";
+                var rJson = JSON.parse(data);
+                var pre = '<gpx><trk>';
+                var track = '';
+                rJson.coordinates.items.forEach(function (point) {
+                    track += '<trkpt lat="' + point.lat + '" lon="' + point.lng + '"><ele>' + point.alt + '</ele></trkpt>';
+                })
+                var past = '</trk></gpx>';
+                addToPage(pre + track + past);
+                console.log(pre + track + past);
+            }
+        }
+    }
+}
+
+function addToPage(gpx) {
+    var aTags = document.getElementsByTagName("li");
+    var searchText = "Für GPS-Gerät herunterladen";
+    var found;
+
+    for (var i = 0; i < aTags.length; i++) {
+        if (aTags[i].textContent == searchText) {
+            found = aTags[i];
+
+            break;
+        }
+    }
+
+    var newListitem = document.createElement("div");
+    var newTextarea = document.createElement("textarea");
+    newTextarea.value = gpx;
+
+    newListitem.appendChild(newTextarea);
+
+    found.parentNode.insertBefore(newListitem, found);
+
+    found.parentNode.appendChild(newListitem);
+    var body = document.getElementsByTagName('body');
+    body[0].appendChild(newListitem);
+
+
+}
