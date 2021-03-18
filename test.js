0285fb9da8574d3c7e2b3210917233a4cee9a6db
Dominik Herold <dominik.herold@web.de>
2021-03-18 23:51:09 +0100

    convert to bookmarklet

--- test.js
+++ test.js
@@ -1,11 +1,6 @@
-window.onload = function () {
-    ripData();
-    
-//    document.getElementById('rip').addEventListener('click', goRip);pop
-}
-
+javascript:(function() {
 function ripData(){
-    var scripts = document.getElementsByTagName("script");
+   var scripts = document.getElementsByTagName("script");
     var findRe = /(kmtBoot.setProps)/;
     var jsonRe = /kmtBoot.setProps\("(.*)"\)/;
     for (i = 0; i < scripts.length; i++) {
@@ -13,7 +8,6 @@ function ripData(){
         if (test && test.length > 0)
         {
             var jsonTest = scripts[i].innerHTML.match(jsonRe);
-            
             
             if (jsonTest && jsonTest.length > 0) {
                 var myjson = jsonTest[1];
@@ -23,44 +17,31 @@ function ripData(){
                 myjson = myjson.replace(/"{/g, '{');
                 myjson = myjson.replace(/(}"),"d/g, '},"d');
                 myjson = myjson.replace(/\\\\"/g, '"');
-                var mydata = myjson.match(/"coordinates":{"items":\[(.*?)]/g)
+                var mydata = myjson.match(/"coordinates":{"items":\[(.*?)]/g);
                 var data = "{" + mydata[0]+ "}}";
                 var rJson = JSON.parse(data);
                 var pre = '<gpx><trk>';
                 var track = '';
                 rJson.coordinates.items.forEach(function (point) {
                     track += '<trkpt lat="' + point.lat + '" lon="' + point.lng + '"><ele>' + point.alt + '</ele></trkpt>';
-                })
+                });
                 var past = '</trk></gpx>';
                 addToPage(pre + track + past);
-//                console.log(pre + track + past);
-                
             }
         }
     }
-    return pre + track + past;
+	return pre + track + past;
 }
-
-function goRip(){
-    
-    var myTextArea = document.getElementById('gpx');
-    myTextArea.value = ripData();
-    console.log("rip");
-}
-
 function addToPage(gpx) {
     var aTags = document.getElementsByTagName("li");
     var searchText = "Für GPS-Gerät herunterladen";
     var found =[];
 
     for (var i = 0; i < aTags.length; i++) {
-        if (aTags[i].textContent == searchText) {
-            found.push( aTags[i]);
-
-//            break;
+		if (aTags[i].textContent.indexOf(searchText) > -1) {
+            found.push( aTags[i]);			
         }
     }
-//    console.log(found);
 
     var newListitem = document.createElement("li");
     var newTextarea = document.createElement("textarea");
@@ -69,16 +50,13 @@ function addToPage(gpx) {
     newListitem.appendChild(newTextarea);
     
     for (var i = 0; i < found.length; i++) {
-//        found[i].parentNode.insertBefore(newListitem, found[i]);
         found[i].parentNode.appendChild(newListitem);
     }
     
     setTimeout(function(){ 
 
-    }, 3000)
-
-//    var body = document.getElementsByTagName('body');
-//    body[0].appendChild(newListitem);
-
-
+    }, 3000);
 }
+
+ripData();
+})();
